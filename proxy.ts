fdf7deb0bf3e0a44a96e4d78ec10745c39742fa1
Brillian Alif <brillianalif28@gmail.com>
2026-07-13 12:21:24 +0700

    Update: Clear

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   // 1. Ambil respons default
   const response = NextResponse.next();
 
